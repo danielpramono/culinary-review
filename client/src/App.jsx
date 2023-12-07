e6fce9412c6d08402e71f12738eeafe3375f6234
daniel <danielpramonoaji@gmail.com>
2023-12-07 22:42:35 +0700

    meambah router dan membuat pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SingUp from "./pages/SingUp";
 
 export default function App() {
-  return <div>App</div>;
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Profile" element={<Profile/>}/>
+      <Route path="/SignIn" element={<SignIn/>}/>
+      <Route path="/SignUp" element={<SingUp/>}/>
+    </Routes>
+  </BrowserRouter>
 }
