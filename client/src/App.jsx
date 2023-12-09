9de8776f19dcd3af71bf654a82440540d7e310f1
daniel <danielpramonoaji@gmail.com>
2023-12-09 12:59:50 +0700

    membuat backend dan server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SingUp from "./pages/SingUp";
+import Header from "./components/Header";
 
 export default function App() {
   return <BrowserRouter>
+  <Header />
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/Profile" element={<Profile/>}/>
