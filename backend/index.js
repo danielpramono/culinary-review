d8929d18a0ca39321aa03516ae20c379fcc2ac2e
daniel <danielpramonoaji@gmail.com>
2023-12-09 15:53:07 +0700

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
 const app = express()
 const port = 3000
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('MongoDB Connected');
 })
+.catch((err) => {
+    console.log(err);
+});
 
 app.listen(port, () => {
   console.log(`Sever running on port ${port}`)
