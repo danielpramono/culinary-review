9fcccb984c7c7444b6f432889b474c6aecd32c2a
daniel <danielpramonoaji@gmail.com>
2023-12-10 13:46:23 +0700

    membuat signup user

--- backend/controllers/signupController.js
+++ backend/controllers/signupController.js
@@ -1,3 +1,17 @@
-export const signup = (req, res) => {
-    console.log(req.body);
+import User from '../models/userModel.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const {username, email, password} = req.body
+    const hashedPass = bcryptjs.hashSync(password, 10)
+    try {
+        const newUser = new User({username, email, password: hashedPass});
+        await newUser.save();
+        res.status(200).send({success: true, massage: "Berhasil menambahkan user"})
+    } catch (err) {
+        if (err.name === 'MongoServerError' && err.code === 11000){
+            return res.status(400).json({success: false ,message: "User already exist", err})
+        }
+        return res.status(401).send({success: false, message: "Error saat menambahkan user", err})
+    }
 };
