3287dda2b9e65756c380c204f019edc2f17bb9d5
daniel <danielpramonoaji@gmail.com>
2023-12-10 15:10:58 +0700

    membuat signup di client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import SingUp from "./pages/SingUp";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return <BrowserRouter>
@@ -12,7 +12,7 @@ export default function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/Profile" element={<Profile/>}/>
       <Route path="/SignIn" element={<SignIn/>}/>
-      <Route path="/SignUp" element={<SingUp/>}/>
+      <Route path="/SignUp" element={<SignUp/>}/>
     </Routes>
   </BrowserRouter>
 }
